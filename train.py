796864a3602a3cfbaa8139f3240305504729d41d
hariharan-palanichamy <hariharanp1094@gmail.com>
2020-10-23 06:48:09 +0530

    train script changes commit

--- train.py
+++ train.py
@@ -16,15 +16,6 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 
 ds = TabularDatasetFactory.from_delimited_files("https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
 
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
-
-run = Run.get_context()
-
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -52,7 +43,15 @@ def clean_data(data):
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
 
     return x_df , y_df
-    
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+
+### YOUR CODE HERE ###a
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+
+run = Run.get_context()   
 
 def main():
     # Add arguments to script
